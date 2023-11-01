b64b40946917e88f25b9f1c2ecc836a74d0f3711
stephanp123 <145160503+stephanp123@users.noreply.github.com>
2023-11-01 08:20:26 -0400

    Added test file, pending tests

---
+++ RPSTests/RPSTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RPSTests
+{
+	TEST_CLASS(RPSTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+
+
+
+		}
+	};
+}
